62912e49d2ed98a70b56842c353a9ec6b9e22006
laishengyuan <lsyuan.36@gmail.com>
2025-12-02 17:43:11 +0800

    [新增] GitHub Actions 工作流程以部署 Vue 應用至 GitHub Pages，更新 .gitignore 以排除 .venv，並設置 Vite 基本路徑

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // ↓↓↓ 請修改這裡：填入你的 GitHub Repository 名稱 (前後都要有斜線) ↓↓↓
+  base: '/cogi/',
+  
   plugins: [
     vue(),
   ],
